a37d4b37e118c5331a2bc30035220103d72a63df
Francis Bukenya <frankie.bukenya@gmail.com>
2020-07-20 22:38:12 +0200

    WIP basic tetris monday night

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function collide(arena,player) {
+  const [m, o] = [player.matrix, player.pos]
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && arena[y + 0.y] && arena[y+ 0.y] ) {
+        
+      }
+    }
+  }
+}
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -29,6 +48,21 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x]= value;
+            }
+        });
+    });
+}
+
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
 let dropCounter = 0;
 const dropInterval = 1000;
 
@@ -40,15 +74,30 @@ function update(time = 0) {
     dropCounter += deltatime;
     if (dropCounter > dropInterval) {
         player.pos.y++;
+
+        dropCounter = 0;
     }
 
     draw();
     requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
     pos: { x: 5, y: 5 },
     matrix,
 };
+
+document.addEventListener('keydown', event => {
+    // pomle.github.io/keycode
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39) {
+        Player.pos.x++;
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+});
 
 update();
