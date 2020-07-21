596481f0d44e5ed7b4f52b8f9c298749e6b4afc7
Francis Bukenya <frankie.bukenya@gmail.com>
2020-07-21 12:13:13 +0200

    Add styling

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+  let rowCount = 1
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+
+      player.score += rowCount * 10
+      rowCount *= 2
+    }
+}
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
@@ -111,7 +128,8 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
-        player.pos.y = 0;
+      arenaSweep();
+      updateScore();
     }
     dropCounter = 0;
 }
@@ -129,9 +147,11 @@ function playerReset() {
     player.pos.y = 0;
     player.pos.x = ((arena[0].length / 2) | 0) - ((player.matrix[0].length / 2) | 0);
 
-  if (collide(arena, player)) {
-    arena.forEach(row => row.fill(0))
-  }
+    if (collide(arena, player)) {
+      arena.forEach(row => row.fill(0));
+      player.score = 0;
+      updateScore()
+    }
 }
 
 function playerRotate(dir) {
@@ -182,22 +202,18 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
-const colors = [
-  null,
-  '#ff0d72',
-  '#0dc2ff',
-  '#0dff72',
-  '#f538ff',
-  '#ff8e0d',
-  '#ffe138',
-  '#3877ff'
-]
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
+const colors = [null, '#ff0d72', '#0dc2ff', '#0dff72', '#f538ff', '#ff8e0d', '#ffe138', '#3877ff'];
 
 const arena = createMatrix(12, 20);
 
 const player = {
-    pos: { x: 5, y: 5 },
-    matrix: createPiece('T'),
+    pos: { x: 0, y: 0 },
+    matrix: null,
+    score: 0,
 };
 
 document.addEventListener('keydown', event => {
@@ -215,4 +231,6 @@ document.addEventListener('keydown', event => {
     }
 });
 
+playerReset();
+updateScore();
 update();
