b74e000de1f8a296c680f5c9e405f2ac6fdd49f3
Francis Bukenya <frankie.bukenya@gmail.com>
2020-08-01 15:34:07 +0200

    Fix connection to websocket

--- server/main.js
+++ server/main.js
@@ -1,3 +1,11 @@
 const WebSocketServer = require('ws').Server
 
 const server = new WebSocketServer({port: 9000})  
+
+server.on('connection', conn => {
+  console.log("Connection established ", conn)
+
+  conn.on('closed', () => { 
+    console.log('Connection closed')
+  })
+})
